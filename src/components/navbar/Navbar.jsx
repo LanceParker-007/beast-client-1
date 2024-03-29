9bf81b85578f1407cb8e31452fd09572cd63ee0f
lance <lanceparker2001@gmail.com>
2024-03-29 19:11:03 +0530

    Shifted SignInWithGoogleButton inside the Navbar only

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,10 +7,92 @@ import {
   DrawerOverlay,
   HStack,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
 import React from "react";
-import { Link } from "react-router-dom";
-import SignInWithGoogleButton from "../signInWithGoogleButton/SignInWithGoogleButton.jsx";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { setUser } from "../../redux/slices/authSlice";
+import Cookies from "js-cookie";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
+import { devServer } from "../../index";
+import { RiLogoutBoxLine } from "react-icons/ri";
+// import SignInWithGoogleButton from "../signInWithGoogleButton/SignInWithGoogleButton.jsx";
+
+// SignInWithGoogleButton created here only because getting some error when importing from the other file
+const SignInWithGoogleButton = ({ ...props }) => {
+  const toast = useToast();
+  const { user } = useSelector((state) => state.authSliceReducer);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(setUser(null));
+    Cookies.remove("userInfo");
+  };
+
+  return !user ? (
+    <GoogleLogin
+      onSuccess={async (credentialResponse) => {
+        const credentialResponseDecoded = jwtDecode(
+          credentialResponse.credential
+        );
+        const username = credentialResponseDecoded.name;
+        const email = credentialResponseDecoded.email;
+        const pic = credentialResponseDecoded.picture;
+
+        try {
+          const config = {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          };
+
+          const { data } = await axios.post(
+            `${devServer}/api/v1/user/signin-with-google`,
+            { username, email, pic },
+            config
+          );
+
+          toast({
+            title: "Login successful 👍",
+            status: "success",
+            duration: 5000,
+            isClosable: true,
+            position: "top",
+          });
+
+          //Saving user in local storage
+          dispatch(setUser(data));
+
+          Cookies.set("userInfo", JSON.stringify(data), {
+            expires: 1,
+          });
+          navigate("/");
+        } catch (error) {
+          // console.log(error);
+          toast({
+            title: `${error.message}`,
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+            position: "top",
+          });
+        }
+      }}
+      onError={() => {
+        console.log("Login Failed");
+      }}
+    />
+  ) : (
+    <Button variant="ghost" onClick={handleLogout}>
+      <RiLogoutBoxLine />
+      Logout
+    </Button>
+  );
+};
 
 //Remember to DeStructure
 const LinkButton = ({ url = "/", title = "Home", onClose }) => {
